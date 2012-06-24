362efa1469ed251d348cbca0e0a09ca14b04dd7b
Kelvin Chou <kelvin457@OSX.local>
2012-06-23 20:22:41 -0700

    Added slide and parseHtml

---
+++ testing/parseHtml.py
@@ -0,0 +1,38 @@
+from slide import Slide
+def parseHtml(filename):
+    """ 
+    Parses a filename and adds the divs for impress.js
+    """
+
+    opened = open(filename, 'r')
+    linesToAdd = []
+    new_file = open('tryTest.html', 'w')
+    lines = opened.readlines()
+    x, y, counter = 0, 0, 1
+    currentBody = ''
+    for index in range(len(lines)):
+        if (lines[index][0:4] == '<h1>') :
+            if (currentBody != ''):
+                slideObject = Slide(x, y, currentBody, "slide-" + str(counter))
+                counter += 1
+                x += 1000
+                linesToAdd.append('<div id={0} class={1} data-x={2} data-y={3}>\n'.format(slideObject.title(), slideObject.format(), slideObject.x(), slideObject.y()))
+                linesToAdd.append('<p>\n{0}\n</p>\n</div>\n'.format(slideObject.content()))
+                currentBody = lines[index]
+        else:
+            currentBody += lines[index]
+    if(currentBody != ''):
+        slideObject = Slide(x, y, currentBody, "slide-" + str(counter))
+        counter += 1
+        x += 1000
+        linesToAdd.append('<div id={0} class={1} data-x={2} data-y="{3}">\n'.format(slideObject.title(), slideObject.format(), slideObject.x(), slideObject.y()))
+        linesToAdd.append('<p>\n{0}\n</p>\n</div>'.format(slideObject.content()))
+        currentBody = lines[index]
+    new_file.writelines(linesToAdd)
+    opened.close()
+    new_file.close()
+
+if __name__ == '__main__':
+    testing = parseHtml('result.html')
+    for element in testing:
+        print element 
